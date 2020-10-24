9fa128862c1e48dd0eafd0f5c9c24faae8e28505
ZC <zc@ZCs-MacBook-Air.local>
2020-10-23 22:48:39 -0400

    Added query for games in past week

--- lab08/monopoly-queries.sql
+++ lab08/monopoly-queries.sql
@@ -18,48 +18,49 @@
 -- Get all games that occured in the past week
 SELECT *
   FROM Game
-  -- TODO: compare today's date to those in the past week
+  WHERE time >= NOW() - INTERVAL '7 days'
+  ;
 
 -- Get list of players who have non-NULL names
--- SELECT *
---   FROM Player
---   WHERE name IS NOT NULL
---   ;
+SELECT *
+  FROM Player
+  WHERE name IS NOT NULL
+  ;
 
 -- Get list of IDs for players who have some game score > 2000
--- SELECT playerID
---   FROM PlayerGame
---   WHERE score > 2000
---   ;
+SELECT playerID
+  FROM PlayerGame
+  WHERE score > 2000
+  ;
 
 -- Get list of players who have GMail accounts
--- SELECT *
---   FROM Player
---   WHERE emailAddress LIKE '%gmail%'
---   ;
+SELECT *
+  FROM Player
+  WHERE emailAddress LIKE '%gmail%'
+  ;
 
 ---------------------------
 -- Multiple-Table Queries
 ---------------------------
 
 -- Get all "The King"'s game scores in decreasing order
--- SELECT score
---   FROM Player, PlayerGame
---   WHERE ID = playerID
---     AND name = 'The King'
---   ORDER BY score DESC
---   ;
+SELECT score
+  FROM Player, PlayerGame
+  WHERE ID = playerID
+    AND name = 'The King'
+  ORDER BY score DESC
+  ;
 
 
 -- Get the name of the winner of the game played on 2006-06-28 13:20:00
--- SELECT name
---   FROM Player, Game, PlayerGame
---   WHERE time = '2006-06-28 13:20:00'
---     AND playerID = Player.ID
---     AND gameID = Game.ID
---   ORDER BY score DESC
---   LIMIT 1
---   ;
+SELECT name
+  FROM Player, Game, PlayerGame
+  WHERE time = '2006-06-28 13:20:00'
+    AND playerID = Player.ID
+    AND gameID = Game.ID
+  ORDER BY score DESC
+  LIMIT 1
+  ;
 -- Note: the game winner was 'me@calvin.edu', whose name is NULL;
 --        so the query will return NULL
 
