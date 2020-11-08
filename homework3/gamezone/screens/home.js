2fb70d15e4562831cf5cfc550fe00d9507b7cf5f
ZC <zc@ZCs-MacBook-Air.local>
2020-11-07 21:54:57 -0500

    Added support for database requests

--- homework3/gamezone/screens/home.js
+++ homework3/gamezone/screens/home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, View, Text, TouchableOpacity, FlatList } from 'react-native';
+import { ActivityIndicator, View, Text, FlatList } from 'react-native';
 import { globalStyles } from '../styles/global';
 import Card from '../shared/card';
 
@@ -8,7 +8,7 @@ export default function Home({ navigation }) {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        fetch('https://cs262-monopoly-service.herokuapp.com/players/')
+        fetch('https://cs262-service.herokuapp.com/player_playergame')
             .then((response) => response.json())
             .then((json) => setData(json))
             .catch((error) => console.error(error))
@@ -22,11 +22,14 @@ export default function Home({ navigation }) {
                     data={data} 
                     keyExtractor={({ id }, index) => id.toString()}
                     renderItem={({ item }) => (
-                        <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
-                            <Card>
-                                <Text style={globalStyles.titleText}>{ item.email }</Text>
-                            </Card>
-                        </TouchableOpacity>
+                        <Card>
+                            <Text style={globalStyles.titleText}>Player: { item.name == null ? "N/A" : item.name }</Text>
+                            <Text style={globalStyles.titleText}>Email: { item.emailaddress }</Text>
+                            <Text style={globalStyles.titleText}>Game: { item.gameid }</Text>
+                            <Text style={globalStyles.titleText}>Score: { item.score }</Text>
+                            <Text style={globalStyles.titleText}>{ item.currentlocation == null ? "Game ended" 
+                                                                    : "Currently at " + item.currentlocation }</Text>
+                        </Card>
                     )} 
                 />
             )}
